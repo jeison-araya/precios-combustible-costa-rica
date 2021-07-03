d0428a370e6d3dcef7c5b30e7d63cf9bb07e682b
Jeison Araya Mena <jeison.arayamena@ucr.ac.cr>
2021-07-03 18:38:32 +0000

    First Commit

---
+++ recope-prices/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { HomeComponent } from './components/home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+ 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
