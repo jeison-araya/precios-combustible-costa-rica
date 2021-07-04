650f4a95b6469b31db74a9b1a6ff907982e73f5f
Jeison Araya Mena <jeison.arayamena@ucr.ac.cr>
2021-07-04 17:34:29 +0000

    angular material module added

--- recope-prices/src/app/material.module.ts
+++ recope-prices/src/app/material.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatGridListModule,
   ],
   exports: [
-    MatToolbarModule
+    MatGridListModule
   ]
 })
 export class MaterialModule { }
