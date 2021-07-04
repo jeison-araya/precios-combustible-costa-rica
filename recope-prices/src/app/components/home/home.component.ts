3402d1db544f74855516aa1ba9a546739817e468
Jeison Araya Mena <jeison.arayamena@ucr.ac.cr>
2021-07-04 23:15:07 +0000

    Components added

--- recope-prices/src/app/components/home/home.component.ts
+++ recope-prices/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecopeApiService } from '../../services/recope-api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  consumerProducts: any[] = [];
+  vendorProducts: any[] = [];
+  consumerProductsLoaded = false;
+  vendorProductsLoaded = false;
+
+  constructor(private recopeApi: RecopeApiService) { }
 
   ngOnInit(): void {
-  }
+  this.consumerProductsLoaded = false;
+  this.vendorProductsLoaded = false;
 
+  this.recopeApi.getConsumerPrices().subscribe(response => {
+    this.consumerProducts = response;
+    this.consumerProductsLoaded = true;
+  });
+
+  this.recopeApi.getVendorPrices().subscribe(response => {
+    this.vendorProducts = response;
+    this.vendorProductsLoaded = true;
+  });
+  }
 }
